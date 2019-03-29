87102c8c224305ecc92ed8504a24304678f72098
delfipolito <delfinapolito@gmail.com>
2019-03-29 14:29:13 -0300

    Hero animation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,7 +14,10 @@ class Hero extends React.Component {
         <Navbar />
         <Container>
           <div>
-            <h1>We believe and <span className="pink">fight for freedom</span></h1>
+            <h1 className="fist-title">We believe and</h1>
+            <h1 className="second-title">
+              <span className="pink">fight for freedom</span>
+            </h1>
           </div>
         </Container>
       </HeroSection>
@@ -37,6 +40,121 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-start;
+  .fist-title,
+  .fist-title::after {
+    animation-delay: var(--animation-delay, 1s);
+    animation-iteration-count: var(--iterations, 1);
+    animation-duration: var(--duration, 800ms);
+    animation-fill-mode: both;
+    animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
+  }
+  .second-title, .second-title::after {
+    animation-delay: var(--animation-delay, 1.5s);
+    animation-iteration-count: var(--iterations, 1);
+    animation-duration: var(--duration, 800ms);
+    animation-fill-mode: both;
+    animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
+  }
+
+  .fist-title {
+    position: relative;
+    animation-name: clip-text;
+    white-space: nowrap;
+
+    &::after {
+      content: '';
+      position: absolute;
+      z-index: 999;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: #ff7c56;
+      transform: scaleX(0);
+      transform-origin: 0 50%;
+      pointer-events: none;
+      animation-name: text-revealer;
+    }
+  }
+  .second-title {
+    position: relative;
+    animation-name: clip-text2;
+    white-space: nowrap;
+
+    &::after {
+      content: '';
+      position: absolute;
+      z-index: 999;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: #FF5D5D;
+      transform: scaleX(0);
+      transform-origin: 0 50%;
+      pointer-events: none;
+      animation-name: text-revealer2;
+    }
+  }
+
+  @keyframes clip-text {
+    from {
+      clip-path: inset(0 100% 0 0);
+    }
+    to {
+      clip-path: inset(0 0 0 0);
+    }
+  }
+
+  @keyframes text-revealer {
+    0%,
+    50% {
+      transform-origin: 0 50%;
+    }
+
+    60%,
+    100% {
+      transform-origin: 100% 50%;
+    }
+
+    60% {
+      transform: scaleX(1);
+    }
+
+    100% {
+      transform: scaleX(0);
+    }
+  }
+
+  @keyframes clip-text2 {
+    from {
+      clip-path: inset(0 100% 0 0);
+    }
+    to {
+      clip-path: inset(0 0 0 0);
+    }
+  }
+
+  @keyframes text-revealer2 {
+    0%,
+    50% {
+      transform-origin: 0 50%;
+    }
+
+    60%,
+    100% {
+      transform-origin: 100% 50%;
+    }
+
+    60% {
+      transform: scaleX(1);
+    }
+
+    100% {
+      transform: scaleX(0);
+    }
+  }
+
   h1 {
     font-family: 'FontBold';
     margin: 0;
