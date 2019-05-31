551353f66e6de305543dbeaf87ab6611ad4d8958
delfipolito <delfinapolito@gmail.com>
2019-05-31 09:15:08 -0300

    Resize aracon video

--- src/components/Events.js
+++ src/components/Events.js
@@ -50,7 +50,8 @@ const Event = styled.div`
   ${medium('width: 70%;')};
   iframe {
     width: 100%;
-    height: 23vw;
+    height: 42vw;
+    ${medium('height: 23vw;')};
   }
   h6 {
     margin: 60px 0 20px 0 !important;
